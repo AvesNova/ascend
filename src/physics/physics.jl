0f53b013aac079255037f8a73252f750a68b7ea4
Julian Kizanis <julianzia@gmail.com>
2023-07-21 18:09:54 -0600

    Adding basic physics

--- src/physics/physics.jl
+++ src/physics/physics.jl
@@ -2,30 +2,117 @@ using OrdinaryDiffEq
 
 include("pga.jl")
 
+"""
+    inertia_map(twist::MultiVector, inertia::MultiVector) -> MultiVector
 
+Compute the momentum of a given twist and inertia.
+
+# Arguments
+- `twist::MultiVector`: A PGA Line representing the twist (linear + angular velocity).
+- `inertia::MultiVector`: A PGA Line representing the moment of inertia.
+
+# Returns
+- `MultiVector`: A PGA Line indicating the momentum.
+
+# Example
+'''julia
+twist = LinePGA(0.0, 0.0, 0.0, 0.1, 0.001, 0.0)
+inertia = LinePGA(2, 1, 3, 1, 1, 1)
+inertia_map(twist, inertia)
+'''
+"""
 function inertia_map(twist::MultiVector, inertia::MultiVector)::MultiVector
     return dual(twist) .* inertia
 end
 
+"""
+    inv_inertia_map(momentum::MultiVector, inertia::MultiVector) -> MultiVector
+
+Calculate the twist of a given momentum and moment of inertia.
+
+# Arguments
+
+- `momentum::MultiVector`: A PGA Line representing the momentum.
+- `inertia::MultiVector`: A PGA Line representing the inertia.
+
+# Returns
+
+- `MultiVector`: The PGA Line representing the twist (linear + angular velocity).
+
+# Example
+
+'''julia
+julia> momentum = LinePGA(0.0, 0.001, 0.3, 0.0, 0.0, 0.0)
+julia> inertia = LinePGA(2, 1, 3, 1, 1, 1)
+julia> inv_inertia_map(momentum, inertia)
+'''
+"""
 function inv_inertia_map(momentum::MultiVector, inertia::MultiVector)::MultiVector
     return dual(momentum ./ inertia)
 end
 
+"""
+    Δpose(twist::MultiVector, pose::MultiVector) -> MultiVector
+
+Calculate the change in pose (position + orientation) for a given twist (linear + angular velocity).
+
+# Arguments
+
+- `twist::MultiVector`: A PGA Line representing the twist (linear + angular velocity).
+- `pose::MultiVector`: A PGA Motor representing the current pose (position + orientation).
+
+# Returns
+
+- `MultiVector`: The change in the pose.
+
+# Example
+
+'''julia
+julia> twist = LinePGA(0.0, 0.0, 0.0, 0.1, 0.001, 0.0)
+julia> pose = MotorPGA(-1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0)
+julia> Δpose(twist, pose)
+'''
+"""
 function Δpose(twist::MultiVector, pose::MultiVector)::MultiVector
     return -0.5 * pose * twist
 end
 
+"""
+    Δpose!(Δpose::Vector, twist::Vector, pose::Vector, p, t::Real)
+
+Updates the `Δpose` vector based on the given `twist` and `pose`.
+"""
 function Δpose!(Δpose::Vector, twist::Vector, pose::Vector, p, t::Real)
-    twist_line = LinePGA(twist)
-    pose_motor = MotorPGA(pose)
-    Δpose_line = -0.5 * pose_motor * twist_line
+    twist_line::MultiVector = LinePGA(twist)
+    pose_motor::MultiVector = MotorPGA(pose)
+    Δpose_line::MultiVector = -0.5 * pose_motor * twist_line
     Δpose .= coefficients(Δpose_line, MOTOR_PGA_INDICES_VECTOR)
 end
 
 """
-    Δtwist(twist::MultiVector, inertia::MultiVector; forque::MultiVector = Line(0,0,0,0,0,0))::MultiVector
+    Δtwist(twist::MultiVector, inertia::MultiVector; forque::MultiVector = LinePGA(0,0,0,0,0,0)) -> MultiVector
 
-Acceleration = I⁻¹[Forque + Rate ×₋ I[Rate]]
+Calculate the change in twist given inertia and an optional external forque (force + torque).
+
+Acceleration is computed as I⁻¹[Forque + Twist ×₋ I[Twist]].
+
+# Arguments
+
+- `twist::MultiVector`: A line representing the twist (linear + angular velocity).
+- `inertia::MultiVector`: A line representing the inertia.
+- `forque::MultiVector` (optional): An external force represented as a line. Defaults to zero.
+
+# Returns
+
+- `MultiVector`: The change in the twist.
+
+# Example
+
+'''julia
+julia> twist = LinePGA(0.0, 0.0, 0.0, 0.1, 0.001, 0.0)
+julia> inertia = LinePGA(2, 1, 3, 1, 1, 1)
+julia> Δtwist(twist, inertia)
+'''
 """
 function Δtwist(
     twist::MultiVector, 
@@ -38,17 +125,50 @@ function Δtwist(
     return inv_inertia_map(momentum, inertia)
 end
 
+"""
+    Δtwist!(Δtwist::Vector, twist::Vector, pose::Vector, p, t::Real)::MultiVector
+
+Acceleration = I⁻¹[Forque + Rate ×₋ I[Rate]]
+"""
 function Δtwist!(Δtwist::Vector, twist::Vector, pose::Vector, p, t::Real)
-    inertia = LinePGA(2, 1, 3, 1, 1, 1)
-    twist_line = LinePGA(twist)
+    inertia::MultiVector = p.inertia
+    twist_line::MultiVector = LinePGA(twist)
 
     Itwist::MultiVector = inertia_map(twist_line, inertia)
-    momentum::MultiVector = twist_line ×₋ Itwist
-    result_Δtwist = inv_inertia_map(momentum, inertia)
+    momentum::MultiVector = twist_line ×₋ Itwist + p.forque(twist, pose, t)
+    Δtwist_line::MultiVector = inv_inertia_map(momentum, inertia)
 
-    Δtwist .= coefficients(result_Δtwist, LINE_PGA_INDICES_VECTOR)
+    Δtwist .= coefficients(Δtwist_line, LINE_PGA_INDICES_VECTOR)
 end
 
+"""
+    euler_step(twist::MultiVector, pose::MultiVector, inertia::MultiVector, step_count::Int, dt::Real) -> Tuple{MultiVector, MultiVector}
+
+Perform an Euler integration step on the pose and twist.
+
+# Arguments
+
+- `twist::MultiVector`: A PGA Line representing the twist.
+- `pose::MultiVector`: A PGA Motor representing the current pose.
+- `inertia::MultiVector`: A PGA Line representing the inertia.
+- `step_count::Int`: Number of steps in the integration.
+- `dt::Real`: Time step size.
+
+# Returns
+
+- `Tuple{MultiVector, MultiVector}`: The updated twist and pose after performing the Euler integration.
+
+# Example
+
+'''julia-repl
+julia> twist = LinePGA(0.0, 0.0, 0.0, 0.1, 0.001, 0.0)
+julia> pose = MotorPGA(-1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0)
+julia> inertia = LinePGA(2, 1, 3, 1, 1, 1)
+julia> step_count = 1000
+julia> dt = 1.0
+julia> euler_step(twist, pose, inertia, step_count, dt)
+'''
+"""
 function euler_step(twist::MultiVector, pose::MultiVector, inertia::MultiVector, step_count::Int, dt::Real)::Tuple{MultiVector, MultiVector}
     for _ in 1:step_count
         twist += Δtwist(twist, inertia) * dt / step_count
@@ -59,49 +179,86 @@ function euler_step(twist::MultiVector, pose::MultiVector, inertia::MultiVector,
     return twist, pose
 end
 
-function init_rigid_body_motion(twist::Vector, pose::Vector, inertia::MultiVector; forque::Function)::SciMLBase.DynamicalODEProblem
-    # TODO: add inertia and forque into p
-    return DynamicalODEProblem(Δtwist!, Δpose!, twist, pose, tspan)
+"""
+    init_rigid_body_motion(twist::Vector, pose::Vector, inertia::MultiVector; forque::Function)::SciMLBase.DynamicalODEProblem
+
+Initialize a dynamical ODE problem for rigid body motion.
+
+# Arguments
+- `twist::Vector`: Initial twist coefficients.
+- `pose::Vector`: Initial pose coefficients.
+- `inertia::MultiVector`: Moment of inertia as a PGA Line.
+- `forque::Function`: A function returning external force/torque as a PGA Line (defaults to a zero function).
+
+# Returns
+- A `DynamicalODEProblem` for the given parameters.
+"""
+function RigidBodyMotionProblem(
+    twist::Vector,
+    pose::Vector,
+    inertia::MultiVector,
+    Δt::Real;
+    forque::Function=((pose, t) -> LinePGA(0,0,0,0,0,0))
+)
+    pose_motor::MultiVector = MotorPGA(pose)
+    pose_motor /= norm(pose_motor)
+    pose = coefficients(pose_motor, MOTOR_PGA_INDICES_VECTOR)
+
+    p = (inertia=inertia, forque=forque)
+    return DynamicalODEProblem(Δtwist!, Δpose!, twist, pose, (0.0, Δt), p)
 end
 
-function step!(
-    ode_problem::SciMLBase.DynamicalODEProblem,
-    Δt::Real,
-)   
-    ode_problem.tspan = (0.0, Δt)
-    sol = solve(ode_problem, Tsit5())
+"""
+    step(twist::Vector, pose::Vector, inertia::MultiVector, Δt::Real; forque::Function) -> Tuple{Vector, Vector}
 
-    ode_problem.twist = sol[end].x[1]
+Perform one step of the rigid body motion simulation without modifying the input vectors.
 
-    pose = MotorPGA(sol[end]x.[2])
-    pose /= norm(pose)
-    ode_problem.pose .= coefficients(pose, MOTOR_PGA_INDICES_VECTOR)
+# Arguments
+- `twist::Vector`: Initial twist coefficients.
+- `pose::Vector`: Initial pose coefficients.
+- `inertia::MultiVector`: Moment of inertia as a PGA Line.
+- `Δt::Real`: Time step for the simulation.
+- `forque::Function`: (Optional) A function returning external forque (force + torque) as a PGA Line. Defaults to zero forque.
+
+# Returns
+- A tuple containing the resulting twist and pose vectors after the simulation step.
+"""
+function rbm_physics_step(
+    twist::Vector,
+    pose::Vector,
+    inertia::MultiVector,
+    Δt::Real;
+    forque::Function=((twist, pose, t) -> LinePGA(0,0,0,0,0,0))
+)::Tuple{Vector{Float64}, Vector{Float64}}
+    pose_motor::MultiVector = MotorPGA(pose)
+    pose_motor /= norm(pose_motor)
+    pose = coefficients(pose_motor, MOTOR_PGA_INDICES_VECTOR)
+
+    p = (inertia=inertia, forque=forque)
+    rbm_prob = DynamicalODEProblem(Δtwist!, Δpose!, twist, pose, (0.0, Δt), p)
+    sol = solve(rbm_prob, Tsit5())
+
+    return sol[end].x[1], sol[end].x[2]
 end
 
-twist_0::MultiVector = LinePGA(0.0, 0.0, 0.0, 0.1, 0.001, 0.0)
-pose_0::MultiVector = MotorPGA(-1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0)
-inertia_0::MultiVector = LinePGA(2, 1, 3, 1, 1, 1)
+# function rbm_physics_step(
+#     twist::MultiVector,
+#     pose::MultiVector,
+#     inertia::MultiVector,
+#     Δt::Real;
+#     forque::Function=((twist, pose, t) -> LinePGA(0,0,0,0,0,0))
+# )::Tuple{MultiVector, MultiVector}
+#     pose /= norm(pose)
 
-twist_1, pose_1 = euler_step(twist_0, pose_0, inertia_0, 1000000, 100)
-twist_e = coefficients(twist_1) |> collect
-pose_e = coefficients(pose_1) |> collect
+#     twist_vector = coefficients(twist, LINE_PGA_INDICES_VECTOR)
+#     pose_vector = coefficients(twist, MOTOR_PGA_INDICES_VECTOR)
 
-twist_T::Vector{Float64} = [0.0, 0.0, 0.0, 0.1, 0.001, 0.0]
-pose_T::Vector{Float64} = [-1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0]
-inertia_T::Vector{Float64} = [2, 1, 3, 1, 1, 1]
+#     p = (inertia=inertia, forque=forque)
+#     rbm_prob = DynamicalODEProblem(Δtwist!, Δpose!, twist_vector, pose_vector, (0.0, Δt), p)
+#     sol = solve(rbm_prob, Tsit5())
 
-u₀::Vector{Float64} = [pose_T; twist_T]
+#     twist = LinePGA(sol[end].x[1])
+#     pose = MotorPGA(sol[end].x[2])
 
-tspan = (0.0, 100.0)
-
-prob = DynamicalODEProblem(Δtwist!, Δpose!, twist_T, pose_T, tspan)
-
-sol = solve(prob, Tsit5())
-
-print(sol[end])
-
-twist_t = sol[end][1:6]
-pose_t = sol[end][7:end]
-
-@show norm(twist_e - twist_t)
-@show norm(pose_e - pose_t)
+#     return twist, pose
+# end
---
+++ tests/physics_tests.jl
@@ -0,0 +1,117 @@
+using Test
+
+include("../src/physics/physics.jl")
+
+@testset "Physics Tests" begin
+    @testset "Rigid Body Motion Tests" begin
+        Δt = 100.0
+        inertia_0::MultiVector = LinePGA(2, 1, 3, 1, 1, 1)
+
+        @testset "Bi-Stable Rotation Test" begin
+            twist_0::MultiVector = LinePGA(0.0, 0.0, 0.0, 0.1, 0.001, 0.0)
+            pose_0::MultiVector = MotorPGA(-1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0)
+            
+            twist_1, pose_1 = euler_step(twist_0, pose_0, inertia_0, 1000000, 100)
+            twist_e = coefficients(twist_1) |> collect
+            pose_e = coefficients(pose_1) |> collect
+            
+            twist_T::Vector{Float64} = [0.0, 0.0, 0.0, 0.1, 0.001, 0.0]
+            pose_T::Vector{Float64} = [-1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0]
+            inertia_T::Vector{Float64} = [2, 1, 3, 1, 1, 1]
+            
+            twist_t, pose_t = rbm_physics_step(twist_T, pose_T, inertia_0, Δt)
+            
+            @test norm(twist_e - twist_t) < 1e-4
+            @test norm(pose_e - pose_t) < 1e-4
+        end
+
+        @testset "Zero Dynamics Test" begin
+            twist_0 =[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+            pose_0 = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+            twist_t, pose_t = rbm_physics_step(twist_0, pose_0, inertia_0, Δt)
+            @test twist_t == twist_0
+            @test pose_t == pose_0
+        end
+
+        @testset "Inertia Invariance Test" begin
+            twist_0 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+            pose_0 = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0]
+            twist_t, _ = rbm_physics_step(twist_0, pose_0, inertia_0, Δt)
+            @test twist_t == twist_0
+        end
+
+        @testset "Normalization Test" begin
+            twist_0 = [0.0, 0.0, 0.0, 0.1, 0.001, 0.0]
+            pose_0 = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0]
+            _, pose_t = rbm_physics_step(twist_0, pose_0, inertia_0, Δt)
+            pose_motor::MultiVector = MotorPGA(pose_t)
+            @test isapprox(norm(pose_motor), 1.0, atol=1e-4)
+        end
+
+        @testset "Symmetry Test" begin
+            twist_0 = [0.0, 0.0, 0.0, 0.1, 0.001, 0.0]
+            pose_0 = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0]
+
+            twist_half, pose_half = rbm_physics_step(twist_0, pose_0, inertia_0, Δt/2)
+            twist_full, pose_full = rbm_physics_step(twist_half, pose_half, inertia_0, Δt/2)
+            
+            twist_single, pose_single = rbm_physics_step(twist_0, pose_0, inertia_0, Δt)
+
+            @test isapprox(twist_full, twist_single, atol=1e-4)
+            @test isapprox(pose_full, pose_single, atol=1e-4)
+        end
+
+        @testset "Reversability Test" begin
+            twist_0 = [0.0, 0.0, 0.0, 0.1, 0.001, 0.0]
+            pose_0 = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, -2.0]
+            
+            twist_single, pose_single = rbm_physics_step(twist_0, pose_0, inertia_0, Δt)
+            twist_reversed, pose_reversed = rbm_physics_step(twist_single, pose_single, inertia_0, -Δt)
+            
+            norm(twist_0 - twist_reversed) < 1e-4
+            norm(pose_0 - pose_reversed) < 1e-4
+        end
+    end
+
+    @testset "Inertial Mapping Test" begin
+        @testset "Inertia Map Inverse Test" begin
+            twists = [
+                LinePGA(0.0, 0.0, 0.0, 0.1, 0.001, 0.0),
+                LinePGA(0.1, 0.2, 0.3, 0.1, 0.2, 0.3),
+                LinePGA(1.0, 1.0, 1.0, 1.0, 1.0, 1.0),
+                LinePGA(0.5, -0.5, 0.5, -0.5, 0.5, -0.5)
+            ]
+            
+            inertias = [
+                LinePGA(2, 1, 3, 1, 1, 1),
+                LinePGA(1, 2, 1, 2, 1, 2),
+                LinePGA(0.5, 0.5, 0.5, 0.5, 0.5, 0.5),
+                LinePGA(3, 2, 1, 3, 2, 1)
+            ]
+
+            for (twist, inertia) in zip(twists, inertias)
+                # Map twist to momentum using inertia_map
+                momentum = inertia_map(twist, inertia)
+
+                # Map momentum back to twist using inv_inertia_map
+                twist_back = inv_inertia_map(momentum, inertia)
+
+                # Adding test conditions
+                @test norm(vector(twist - twist_back)) < 1e-15
+            end
+        end
+
+        @testset "Error Propagation" begin
+            twist = LinePGA(0.1, 0.2, 0.3, 0.4, 0.5, 0.6)
+            small_error = 1e-6
+            twist_with_error = twist + LinePGA(small_error, small_error, small_error, small_error, small_error, small_error)
+            
+            inertia = LinePGA(1, 2, 3, 4, 5, 6)
+            
+            momentum1 = inertia_map(twist, inertia)
+            momentum2 = inertia_map(twist_with_error, inertia)
+            
+            @test norm(vector(momentum1 - momentum2)) < 1e7
+        end
+    end
+end
